148e7a3896bf94aa24d7b917d1d20c92c99ed02c
luu-alex <98a.lexluu@gmail.com>
2024-10-03 15:55:54 -0400

    update tests

--- test/fixtures.ts
+++ test/fixtures.ts
@@ -1,4 +1,6 @@
 
 export const collectionAddress = "0xd9bf94c6f8a55115943c4a26b88463fe32dc5b7a";
-export const collectionID = "b1d85dae-6d6a-4f25-804a-7b9f28799764";
+export const collectionId = "b1d85dae-6d6a-4f25-804a-7b9f28799764";
 export const marketplaceAddress = "0xcb9538e34467f5f19df14fd4588a0aea97590367";
+export const marketplaceId = "036f4777-0fe0-4962-ab1b-93324df9539d";
+export const customProjectId = "d8f758ea-02df-4023-b96f-67ed771489ca";
--- test/integration/marketplace.test.ts
+++ test/integration/marketplace.test.ts
@@ -1,7 +1,7 @@
 import Web3 from 'web3';
 import * as dotenv from 'dotenv';
 import { Web3MarketplacePlugin, marketplaceABI, marketplaceContract } from '../../src';
-import { marketplaceAddress, collectionAddress } from '../fixtures';
+import { marketplaceAddress, marketplaceId, collectionAddress, customProjectId, collectionId } from '../fixtures';
 
 dotenv.config();
 
@@ -19,17 +19,32 @@ describe('Web3MarketplacePlugin method Tests', () => {
         contract = new web3.eth.Contract(marketplaceABI, marketplaceAddress);
     });
 
-    it('should list an item on the marketplace', async () => {
+    it.skip('should list an item on the marketplace', async () => {
         const deadline = (Math.floor(Date.now() / 1000) + 7 * 24 * 60 * 60).toString();
     
-        const receipt = await web3.marketplace.listItem(contract,collectionAddress, "0", "1", deadline);
+        const receipt = await web3.marketplace.listItem(contract,collectionAddress, "1", "1", deadline);
         expect(receipt).toBeDefined();  
     });
 
     it('should cancel an item on the marketplace', async () => {
     
-        const receipt = await web3.marketplace.cancelListing(contract, "0");
+        const receipt = await web3.marketplace.cancelListing(contract, "1");
         expect(receipt).toBeDefined();  
+    });
+
+    it('should get items from marketplace', async () => {
+        const result = await web3.marketplace.getMarketplaceItems(marketplaceId, customProjectId);
+        expect(result.items.length).toBeGreaterThan(0);
+    });
+
+    it('should get collection token', async () => {
+        const result = await web3.marketplace.getCollectionToken(customProjectId, collectionId, "0");
+        expect(result.token_id).toEqual('0');
+    });
+
+    it('should get token owners', async () => {
+        const result = await web3.marketplace.getTokenOwners(customProjectId, collectionId, "0");
+        expect(result.owners).toBeDefined();
     });
     
 });
